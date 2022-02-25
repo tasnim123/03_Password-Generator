1ea9955ceec4099e3f70ec8fa2a77d61ff96f231
tasnim123 <tasnimnafi123@gmail.com>
2022-02-25 14:19:29 -0500

    No change

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ generateEl.addEventListener('click',()=>{
     const hasLower = lowercaseEl.checked
     const hasUpper = uppercaseEl.checked
     const hasNumber = numberEl.checked
+    
     const hasSymbol = symbolEl.checked
 
     resultEl.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol,length)
